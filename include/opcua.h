afc202f3a0e60893f8dd5444f76660f72f250db2
Mark Riddoch <mark@dianomic.com>
2021-07-22 17:21:36 +0000

    OPCUA Plugin based on S2OPC
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

---
+++ include/opcua.h
@@ -0,0 +1,139 @@
+#ifndef _OPCUA_H
+#define _OPCUA_H
+/*
+ * Fledge south service plugin
+ *
+ * Copyright (c) 2021 Dianomic Systems
+ *
+ * Released under the Apache 2.0 Licence
+ *
+ * Author: Amandeep Singh Arora, Mark Riddoch
+ */
+#include <string>
+#include <reading.h>
+#include <logger.h>
+#include <mutex>
+#include <stdlib.h>
+#include <map>
+extern "C" {
+#include "libs2opc_client_cmds.h"
+};
+
+using namespace std;
+
+class OpcUaClient;
+
+/* Lifetime Count of subscriptions */
+#define MAX_LIFETIME_COUNT 1000
+/* Number of targetted publish token */
+#define PUBLISH_N_TOKEN 2
+/* Connection global timeout */
+#define TIMEOUT_MS 10000
+/* Secure Channel lifetime */
+#define SC_LIFETIME_MS 3600000
+
+/* Path to the certificate authority */
+#define PATH_CACERT_PUBL "/home/nerd039/dev/S2OPC/bin/trusted/cacert.der"
+/* Path to the CA CRL */
+#define PATH_CACRL_PUBL "/home/nerd039/dev/S2OPC/bin/revoked/cacrl.der"
+/* Path to the server certificate */
+#define PATH_SERVER_PUBL "/home/nerd039/dev/S2OPC/bin/server_public/server_2k_cert.der"
+/* Path to the client certificate */
+#define PATH_CLIENT_PUBL "/home/nerd039/dev/S2OPC/bin/client_public/client_2k_cert.der"
+/* Path to the client private key */
+#define PATH_CLIENT_PRIV "/home/nerd039/dev/S2OPC/bin/client_private/client_2k_key.pem"
+
+/*
+typedef enum {
+    SOPC_SecurityPolicy_None_URI,
+    SOPC_SecurityPolicy_Basic256_URI,
+    SOPC_SecurityPolicy_Basic256Sha256_URI
+} SOPC_SecurityPolicy_type;
+*/
+
+class OPCUA
+{
+    public:
+        OPCUA(const std::string& url);
+        ~OPCUA();
+        void        clearSubscription();
+        void        addSubscription(const std::string& parent);
+        int         addSubscriptions(std::vector<std::string> vec);
+        void        getEndpoints();
+        void        setAssetName(const std::string& name);
+        void        restart();
+        void        newURL(const std::string& url) { m_url = url; };
+        void        start();
+        void        stop();
+        void        ingest(std::vector<Datapoint *> points, long user_ts);
+        void        setReportingInterval(long value);
+        void        registerIngest(void *data, void (*cb)(void *, Reading))
+                {
+                    m_ingest = cb;
+                    m_data = data;
+                }
+        void        setSecMode(const std::string& secMode);
+        void        setSecPolicy(const std::string& secPolicy);
+        void        setAuthPolicy(const std::string& authPolicy) { m_authPolicy = authPolicy; }
+        void        setUsername(const std::string& username) { m_username = username; }
+        void        setPassword(const std::string& password) { m_password = password; }
+        void        setCaCert(const std::string& cert) { m_certAuth = cert; }
+        void        setServerCert(const std::string& cert) { m_serverPublic = cert; }
+        void        setClientCert(const std::string& cert) { m_clientPublic = cert; }
+        void        setClientKey(const std::string& key) { m_clientPrivate = key; }
+        void        setRevocationList(const std::string& cert) { m_caCrl = cert; }
+	void        dataChange(const char *nodeId, const SOPC_DataValue *value);
+	void	    disconnect();
+    private:
+
+	class Node
+	{
+		public:
+				Node(uint32_t connId, const std::string& nodeId);
+				std::string	getBrowseName() { return m_browseName; };
+				uint32_t	getType() { return m_type; };
+				std::string	getNodeId() { return m_nodeID; };
+		private:
+				const std::string	m_nodeID;
+				std::string		m_browseName;
+				uint32_t		m_type;
+	};
+    private:
+        int         		subscribe();
+	void			browse(const std::string& nodeId, std::vector<std::string>&);
+	int32_t			m_connectionId;
+	int32_t			m_configurationId;
+        std::vector<std::string>
+				m_subscriptions;	// The user subscriptions
+	std::map<std::string, Node *>
+				m_nodes;		// The nodes being monitored
+        std::string            	m_url;
+        std::string            	m_asset;
+        void                	(*m_ingest)(void *, Reading);
+        void                	*m_data;
+        std::mutex            	m_configMutex;
+        bool                	m_connected;
+        long                	m_reportingInterval;
+        
+        std::string         	m_secPolicy;
+        OpcUa_MessageSecurityMode m_secMode;
+
+        std::string            	m_authPolicy;
+        std::string            	m_username;
+        std::string         	m_password;
+        
+        std::string            	m_serverPublic;
+        std::string            	m_clientPublic;
+        std::string            	m_clientPrivate;
+        std::string            	m_certAuth;
+        std::string            	m_caCrl;
+        
+        int64_t             	m_publishPeriod;
+        uint16_t             	m_tokenTarget;
+        int                 	m_nodeIdsSize;
+		
+        bool                 	m_disableCertVerif;
+        uint32_t             	m_maxKeepalive;
+};
+
+#endif
---
+++ requirements.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+##--------------------------------------------------------------------
+## Copyright (c) 2021 Dianomic Systems
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##--------------------------------------------------------------------
+
+##
+## Author: Amandeep Singh Arora, Mark Riddoch
+##
+
+fledge_location=`pwd`
+os_name=`(grep -o '^NAME=.*' /etc/os-release | cut -f2 -d\" | sed 's/"//g')`
+os_version=`(grep -o '^VERSION_ID=.*' /etc/os-release | cut -f2 -d\" | sed 's/"//g')`
+echo "Platform is ${os_name}, Version: ${os_version}"
+
+# mbedtls-dev:
+if [[  $os_name == *"Red Hat"* || $os_name == *"CentOS"* ]]; then
+	echo RHEL/CentOS not currently supported by this plugin
+	exit 1
+else
+	sudo apt-get install -y libmbedtls-dev
+fi
+
+# libexpat:
+git clone https://github.com/libexpat/libexpat.git
+(
+	cd libexpat/expat
+	rm -f CMakeCache.txt
+	mkdir -p build
+	cd build
+	cmake -D CMAKE_INSTALL_PREFIX=/usr/local -D EXPAT_BUILD_PKGCONFIG=ON -D EXPAT_ENABLE_INSTALL=ON -D EXPAT_SHARED_LIBS=ON .. && make -j4 && sudo make install
+)
+
+# libcheck:
+wget https://github.com/libcheck/check/releases/download/0.15.2/check-0.15.2.tar.gz
+tar xf check-0.15.2.tar.gz
+(
+	cd check-0.15.2
+	cp ../check-0.15.2_CMakeLists.txt.patch .
+	patch < check-0.15.2_CMakeLists.txt.patch  # update the CMakeLists.txt file
+	rm -f CMakeCache.txt
+	mkdir -p build
+	cd build
+	cmake .. && make -j4 && sudo make install
+)
+
+# S2OPC
+git clone https://gitlab.com/systerel/S2OPC.git --branch S2OPC_Toolkit_1.1.0
+(
+	cd S2OPC
+	cp ../S2OPC.patch .
+	git apply S2OPC.patch   # apply S2OPC code changes
+	BUILD_SHARED_LIBS=OFF
+	CMAKE_INSTALL_PREFIX=/usr/local
+	./build.sh
+	echo
+	echo "BUILD done, INSTALLING..."
+	echo
+	cd build
+	sudo make install
+)
