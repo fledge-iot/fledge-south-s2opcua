61ac82db33f5bbc6494aaf0822fdbfbfd6570c75
ashish-jabble <ashish@dianomic.com>
2022-05-20 10:15:41 +0530

    requirements.sh and some other fixes
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-##--------------------------------------------------------------------
-## Copyright (c) 2021 Dianomic Systems
+##---------------------------------------------------------------------------
+## Copyright (c) 2022 Dianomic Systems Inc.
 ##
 ## Licensed under the Apache License, Version 2.0 (the "License");
 ## you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 ## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ## See the License for the specific language governing permissions and
 ## limitations under the License.
-##--------------------------------------------------------------------
+##---------------------------------------------------------------------------
 
 ##
 ## Author: Amandeep Singh Arora, Mark Riddoch
@@ -46,7 +46,7 @@ wget https://github.com/libcheck/check/releases/download/0.15.2/check-0.15.2.tar
 tar xf check-0.15.2.tar.gz
 (
 	cd check-0.15.2
-	cp ../fledge-south-s2opcua/check-0.15.2_CMakeLists.txt.patch .
+	cp ../check-0.15.2_CMakeLists.txt.patch .
 	patch < check-0.15.2_CMakeLists.txt.patch  # update the CMakeLists.txt file
 	rm -f CMakeCache.txt
 	mkdir -p build
@@ -58,8 +58,8 @@ tar xf check-0.15.2.tar.gz
 git clone https://gitlab.com/systerel/S2OPC.git
 (
 	cd S2OPC
-	cp ./src/Common/opcua_types/sopc_encodeabletype.h ../fledge-south-s2opcua/include
-	ed ../fledge-south-s2opcua/include/sopc_encodeabletype.h << EOED
+	cp ./src/Common/opcua_types/sopc_encodeabletype.h ../include
+	ed ../include/sopc_encodeabletype.h << EOED
 ,s/typedef const struct SOPC_EncodeableType/typedef struct SOPC_EncodeableType/1
 w
 q
